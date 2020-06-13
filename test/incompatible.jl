ffd4d61709afeae24ab3276585381aa0211298a5
Abel <abel.s.siqueira@gmail.com>
2020-06-13 17:15:22 -0300

    :arrow_up: Support NLPModels 0.13 and update other limits

--- test/incompatible.jl
+++ test/incompatible.jl
@@ -3,10 +3,10 @@ function test_incompatible()
                  :trunk => [:unc],
                  :tron  => [:unc, :bnd])
   problems = Dict(:unc => ADNLPModel(x -> 0, zeros(2)),
-                  :bnd => ADNLPModel(x -> 0, zeros(2), lvar=zeros(2), uvar=ones(2)),
-                  :equ => ADNLPModel(x -> 0, zeros(2), c=x->[0.0], lcon=[0.0], ucon=[0.0]),
-                  :ine => ADNLPModel(x -> 0, zeros(2), c=x->[0.0], lcon=[-Inf], ucon=[0.0]),
-                  :gen => ADNLPModel(x -> 0, zeros(2), c=x->[0.0; 0.0], lcon=[-Inf; 0.0], ucon=zeros(2))
+                  :bnd => ADNLPModel(x -> 0, zeros(2), zeros(2), ones(2)),
+                  :equ => ADNLPModel(x -> 0, zeros(2), x->[0.0], [0.0], [0.0]),
+                  :ine => ADNLPModel(x -> 0, zeros(2), x->[0.0], [-Inf], [0.0]),
+                  :gen => ADNLPModel(x -> 0, zeros(2), x->[0.0; 0.0], [-Inf; 0.0], zeros(2))
                  )
   for (ptype, problem) in problems, (solver, types_accepted) in solvers
     @testset "Testing that $solver on problem type $ptype raises error: " begin
