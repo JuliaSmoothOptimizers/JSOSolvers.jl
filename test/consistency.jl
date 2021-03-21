33f9e709b8f700090fa64692a9ebcd25f6dd2cb6
Abel <abel.s.siqueira@gmail.com>
2021-03-21 11:40:10 -0300

    Change the consistency checks to be faster

--- test/consistency.jl
+++ test/consistency.jl
@@ -1,12 +1,6 @@
 function consistency()
-  unlp = ADNLPModel(x -> begin
-                      sleep(0.1)
-                      (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2
-                    end, zeros(2))
-  unls = ADNLSModel(x -> begin
-                      sleep(0.1)
-                      [x[1] - 1; 10 * (x[2] - x[1]^2)]
-                    end, zeros(2), 2)
+  unlp = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, zeros(2))
+  unls = ADNLSModel(x -> [x[1] - 1; 10 * (x[2] - x[1]^2)], zeros(2), 2)
 
   @testset "Consistency" begin
     args = Pair{Symbol,Number}[:atol => 1e-6, :rtol => 1e-6, :max_eval => 1000, :max_time => 60.0]
@@ -19,7 +13,8 @@ function consistency()
           @test stats.status == :first_order
           reset!(unlp); stats = mtd(unlp; max_eval=1)
           @test stats.status == :max_eval
-          reset!(unlp); stats = mtd(unlp; max_time=0.0)
+          slow_nlp = ADNLPModel(x -> begin sleep(0.1); unlp.f(x); end, unlp.meta.x0)
+          stats = mtd(slow_nlp; max_time=0.0)
           @test stats.status == :max_time
         end
       end
@@ -33,7 +28,8 @@ function consistency()
           @test stats.status == :first_order
           reset!(unls); stats = mtd(unls; max_eval=1)
           @test stats.status == :max_eval
-          reset!(unls); stats = mtd(unls; max_time=0.0)
+          slow_nls = ADNLSModel(x -> begin sleep(0.1); unls.F(x); end, unls.meta.x0, nls_meta(unls).nequ)
+          stats = mtd(slow_nls; max_time=0.0)
           @test stats.status == :max_time
         end
       end
