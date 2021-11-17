a6f471205e0b09d2991644ac2d55da34e09331fc
Monssaf Toukal <tmonssaf@gmail.com>
2021-11-16 19:28:09 -0500

    update benchmarks files

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -3,31 +3,20 @@ using BenchmarkTools
 using LinearAlgebra, Logging, Printf, SparseArrays
 
 # JSO packages
-using Krylov, LinearOperators, NLPModels, NLPModelsModifiers, SolverCore, SolverTools, ADNLPModels, SolverTest, JSOSolvers
+using Krylov, LinearOperators, NLPModels, NLPModelsModifiers, SolverBenchmark, SolverCore, SolverTools, ADNLPModels, SolverTest, JSOSolvers, CUTEst
 
-const SUITE = BenchmarkGroup()
-SUITE[:lbfgs] = BenchmarkGroup()
-n = 30
-D = Diagonal([0.1 + 0.9 * (i - 1) / (n - 1) for i = 1:n])
-A = spdiagm(0 => 2 * ones(n), -1 => -ones(n - 1), -1 => -ones(n - 1))
-
-
-function benchmark_lbfgs()
-  # unconstrained problems found in Solvertest:
-  a₁ = ADNLPModel(x -> (x[1] - 1)^2 + 4 * (x[2] - 1)^2, zeros(2), name = "(x₁ - 1)² + 4(x₂ - 1)²")
-  a₂ = ADNLPModel(x -> dot(x .- 1, D, x .- 1), zeros(n), name = "Diagonal quadratic")
-  a₃ = ADNLPModel(x -> dot(x .- 1, A, x .- 1), zeros(n), name = "Tridiagonal quadratic")
-  a₄ = ADNLPModel(x -> (x[1] - 1)^2 + 100 * (x[2] - x[1]^2)^2, [-1.2; 1.0], name = "Rosenbrock")
-  a₅ = ADNLPModel(
-      x -> sum(100 * (x[i + 1] - x[i]^2)^2 + (x[i] - 1)^2 for i = 1:(n - 1)),
-      collect(1:n) ./ (n + 1),
-      name = "Extended Rosenbrock",
-    )
-
-  ad_problems = [a₁, a₂, a₃, a₄, a₅]
-  for i in 1:5
-    SUITE[:lbfgs][i] = @benchmarkable [lbfgs(nlp) for nlp in $ad_problems]
-  end
+function runcutest(cutest_problems, solvers)
+  return bmark_solvers(solvers, cutest_problems)
 end
 
-benchmark_lbfgs()
+
+problems_names = CUTEst.select(;only_free_var=true, max_con=0, min_var=2, max_var=100)
+cutest_problems = (CUTEstModel(p) for p in problems_names)
+solvers = Dict(
+  :lbfgs =>
+  nlp -> lbfgs(nlp)
+  )
+  
+const SUITE = BenchmarkGroup()
+SUITE[:cutest_lbfgs] = @benchmarkable runcutest(cutest_problems, solvers)
+tune!(SUITE[:cutest_lbfgs])
