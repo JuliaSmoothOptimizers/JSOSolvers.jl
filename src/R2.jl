4e9c1de5636a9051ce8fa6dc0c559e26650a48ad
nathanemac <91251698+nathanemac@users.noreply.github.com>
2022-07-14 18:45:05 -0400

    with D.O comments

--- src/R2.jl
+++ src/R2.jl
@@ -7,18 +7,18 @@ export R2, R2Solver
     A first-order quadratic regularization method for unconstrained optimization
 
 # Arguments
-- `nlp::AbstractNLPModel{T, V}` represents the model to solve, see `NLPModels.jl`.
+- `nlp::AbstractNLPModel{T, V}` is the model to solve, see `NLPModels.jl`.
 
-The keyword arguments may include
-- x::V = nlp.meta.x0`: the initial guess.
-- atol = eps(T)^(1 / 3) : absolute tolerance
-- rtol = eps(T)^(1 / 3) : relative tolerance : algorithm stops when norm_∇fk ≤ ϵ_abs + ϵ_rel*norm_∇fk
-- η1 = T(0.3), η2 = T(0.7) : step acceptance
-- γ1 = T(1/2), γ2 = 1/γ1 : regularization update
-- σmin = eps(T) : step parameter for R2 algorithm
-- max_eval::Int : maximum number of evaluation of the objective function
-- max_time::Float64 = 3600.0: maximum time limit in seconds.
-- verbose::Bool = false : prints information if true 
+# Keyword arguments 
+- x::V = nlp.meta.x0`: the initial guess
+- atol = eps(T)^(1 / 3): absolute tolerance
+- rtol = eps(T)^(1 / 3): relative tolerance: algorithm stop when ||∇f(x)|| ≤ ϵ_abs + ϵ_rel*||∇f(x0)||
+- η1 = T(0.3), η2 = T(0.7): step acceptance parameters
+- γ1 = T(1/2), γ2 = 1/γ1: regularization update parameters
+- σmin = eps(T): step parameter for R2 algorithm
+- max_eval::Int: maximum number of evaluation of the objective function
+- max_time::Float64 = 3600.0: maximum time limit in seconds
+- verbose::Bool = false: prints iteration details if true.
 
 # Output
 The value returned is a `GenericExecutionStats`, see `SolverCore.jl`.
@@ -26,15 +26,15 @@ The value returned is a `GenericExecutionStats`, see `SolverCore.jl`.
 # Examples
 ```jldoctest
 using JSOSolvers, ADNLPModels
-nlp = ADNLPModel(x -> sum(x.^2), ones(3));
+nlp = ADNLPModel(x -> sum(x.^2), ones(3))
 stats = R2(nlp)
 # output
 "Execution stats: first-order stationary"
 ```
 ```jldoctest
 using JSOSolvers, ADNLPModels
-nlp = ADNLPModel(x -> sum(x.^2), ones(3));
-solver = R2Solver(nlp;);
+nlp = ADNLPModel(x -> sum(x.^2), ones(3))
+solver = R2Solver(nlp);
 stats = solve!(solver, nlp)
 # output
 "Execution stats: first-order stationary"
@@ -55,21 +55,20 @@ end
 
 @doc (@doc R2Solver) function R2(
   nlp::AbstractNLPModel{T,V};
-  x::V = nlp.meta.x0,
   kwargs...,
 ) where {T, V}
   solver = R2Solver(nlp)
-  return solve!(solver, nlp; x = x, kwargs...)
+  return solve!(solver, nlp; kwargs...)
 end
 
 function solve!(
     solver::R2Solver{V},
     nlp::AbstractNLPModel{T, V};
-    x::V = copy(nlp.meta.x0),
+    x0::V = copy(nlp.meta.x0),
     atol = eps(T)^(1/3),
     rtol = eps(T)^(1/3),
-    η1 = T(0.3),
-    η2 = T(0.7),
+    η1 = eps(T)^(1/4),
+    η2 = T(0.95),
     γ1 = T(1/2),
     γ2 = 1/γ1,
     σmin = zero(T),
@@ -82,22 +81,20 @@ function solve!(
   start_time = time()
   elapsed_time = 0.0
 
-  solver.x .= copy(x)
+  x = solver.x .= copy(x0)
   ∇fk = solver.gx
   ck = solver.cx
 
   iter = 0
-  ρk = T(0)
   fk = obj(nlp, x)
   
   grad!(nlp, x, ∇fk)
   norm_∇fk=norm(∇fk)
-  σk = 2^round(log2(norm(∇fk) + 1)) # The closest exact-computed power of 2 from ∇x
+  σk = 2^round(log2(norm_∇fk + 1)) # The closest exact-computed power of 2 from ∇fk
 
   # Stopping criterion: 
   ϵ = atol + rtol*norm_∇fk
   optimal = norm_∇fk ≤ ϵ
-  #tired = (neval_obj(nlp) > max_eval) || (elapsed_time > max_time)
   tired = neval_obj(nlp) > max_eval ≥ 0 || elapsed_time > max_time
   if verbose
     @info @sprintf "%5s  %9s  %7s  %7s " "iter" "f" "‖∇f‖" "σ"
@@ -118,14 +115,14 @@ function solve!(
 
     ρk = (fk - fck) / ΔTk 
 
-  # Update regularization parameters
+    # Update regularization parameters
     if ρk >= η2
       σk = max(σmin, γ1 * σk)
     elseif ρk < η1
       σk = σk * γ2
     end
 
-  # Acceptance of the new candidate
+    # Acceptance of the new candidate
     if ρk >= η1
       x .= ck
       fk = fck
@@ -136,10 +133,8 @@ function solve!(
     iter += 1
     elapsed_time = time() - start_time
     optimal = norm_∇fk ≤ ϵ
-    #tired = (neval_obj(nlp) > max_eval) || (elapsed_time > max_time)
     tired = neval_obj(nlp) > max_eval ≥ 0 || elapsed_time > max_time
   
-
     if verbose
       @info infoline
       infoline = @sprintf "%5d  %9.2e  %7.1e  %7.1e" iter fk norm_∇fk σk
@@ -155,8 +150,6 @@ function solve!(
       elseif neval_obj(nlp) > max_eval
         status = :max_eval
       end
-    else
-      :exception
     end
 
   return GenericExecutionStats(
