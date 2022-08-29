e872bb20f98bb8cbaadfa569c06ba71f88e8d56d
nathanemac <91251698+nathanemac@users.noreply.github.com>
2022-08-29 14:45:07 -0400

    remove bug

--- src/R2.jl
+++ src/R2.jl
@@ -186,27 +186,4 @@ function solve!(
   end
 
   return output
-end 
- 19  
-test/callback.jl
-Viewed
-@@ -0,0 +1,19 @@
-using ADNLPModels, JSOSolvers, LinearAlgebra, Logging #, Plots
-@testset "Test callback" begin
-  f(x) = (x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2
-  nlp = ADNLPModel(f, [-1.2; 1.0])
-  X = [nlp.meta.x0[1]]
-  Y = [nlp.meta.x0[2]]
-  function cb(nlp, solver)
-    x = solver.x
-    push!(X, x[1])
-    push!(Y, x[2])
-    if solver.output.iter == 20
-      solver.output.status = :user
-    end
-  end
-  output = with_logger(NullLogger()) do
-    R2(nlp, callback=cb)
-  end
-  @test output.iter == 20
 end
