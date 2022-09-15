a4b3e8b72f30f7773ce0b2c47acb44d46b556fd5
tmigot <tangi.migot@gmail.com>
2022-09-14 20:20:38 -0400

    Review `R2` docstring and callback (#148)
    * review `R2` docstring and callback
    
    * Apply suggestions @dpo
    
    Co-authored-by: Dominique <dominique.orban@gmail.com>
    
    * add additional suggestions
    
    Co-authored-by: Dominique <dominique.orban@gmail.com>

--- src/R2.jl
+++ src/R2.jl
@@ -2,52 +2,66 @@ export R2, R2Solver
 
 """
     R2(nlp; kwargs...)
-    solver = R2Solver(nlp;)
+
+A first-order quadratic regularization method for unconstrained optimization.
+
+For advanced usage, first define a `R2Solver` to preallocate the memory used in the algorithm, and then call `solve!`:
+
+    solver = R2Solver(nlp)
     solve!(solver, nlp; kwargs...)
-    A first-order quadratic regularization method for unconstrained optimization
+
 # Arguments
 - `nlp::AbstractNLPModel{T, V}` is the model to solve, see `NLPModels.jl`.
+
 # Keyword arguments 
 - x0::V = nlp.meta.x0`: the initial guess
 - atol = eps(T)^(1 / 3): absolute tolerance
-- rtol = eps(T)^(1 / 3): relative tolerance: algorithm stop when ||∇f(x)|| ≤ ϵ_abs + ϵ_rel*||∇f(x0)||
+- rtol = eps(T)^(1 / 3): relative tolerance: algorithm stops when ‖∇f(x)‖ ≤ ϵ\\_abs + ϵ\\_rel*‖∇f(x0)‖
 - η1 = eps(T)^(1/4), η2 = T(0.95): step acceptance parameters
 - γ1 = T(1/2), γ2 = 1/γ1: regularization update parameters
 - σmin = eps(T): step parameter for R2 algorithm
 - max_eval::Int: maximum number of evaluation of the objective function
 - max_time::Float64 = 3600.0: maximum time limit in seconds
 - verbose::Int = 0: if > 0, display iteration details every `verbose` iteration.
+
 # Output
 The value returned is a `GenericExecutionStats`, see `SolverCore.jl`.
+
 # Callback
 The callback is called after each iteration.
-The expected signature of the callback is `callback(nlp, solver)`, and its output is ignored.
+The expected signature of the callback is `callback(nlp, solver, stats)`, and its output is ignored.
 Changing any of the input arguments will affect the subsequent iterations.
-In particular, setting `solver.output.status = :user` will stop the algorithm.
+In particular, setting `stats.status = :user` will stop the algorithm.
 All relevant information should be available in `nlp` and `solver`.
 Notably, you can access, and modify, the following:
 - `solver.x`: current iterate;
 - `solver.gx`: current gradient;
-- `solver.output`: structure holding the output of the algorithm (`GenericExecutionStats`), which contains, among other things:
-  - `solver.output.dual_feas`: norm of current gradient;
-  - `solver.output.iter`: current iteration counter;
-  - `solver.output.objective`: current objective function value;
-  - `solver.output.status`: current status of the algorithm. Should be `:unknown` unless the algorithm has found a stopping criteria. Changing this to anything will stop the algorithm, but you should use `:user` to properly indicate the intention.
-  - `solver.output.elapsed_time`: elapsed time in seconds.
+- `stats`: structure holding the output of the algorithm (`GenericExecutionStats`), which contains, among other things:
+  - `stats.dual_feas`: norm of current gradient;
+  - `stats.iter`: current iteration counter;
+  - `stats.objective`: current objective function value;
+  - `stats.status`: current status of the algorithm. Should be `:unknown` unless the algorithm has attained a stopping criterion. Changing this to anything will stop the algorithm, but you should use `:user` to properly indicate the intention.
+  - `stats.elapsed_time`: elapsed time in seconds.
+
 # Examples
 ```jldoctest
 using JSOSolvers, ADNLPModels
 nlp = ADNLPModel(x -> sum(x.^2), ones(3))
 stats = R2(nlp)
+
 # output
+
 "Execution stats: first-order stationary"
 ```
+
 ```jldoctest
 using JSOSolvers, ADNLPModels
 nlp = ADNLPModel(x -> sum(x.^2), ones(3))
 solver = R2Solver(nlp);
 stats = solve!(solver, nlp)
+
 # output
+
 "Execution stats: first-order stationary"
 ```
 """
@@ -102,7 +116,7 @@ function SolverCore.solve!(
 
   grad!(nlp, x, ∇fk)
   norm_∇fk = norm(∇fk)
-  set_residuals!(stats, zero(T), norm_∇fk)
+  set_dual_residual!(stats, norm_∇fk)
 
   σk = 2^round(log2(norm_∇fk + 1))
 
@@ -114,7 +128,7 @@ function SolverCore.solve!(
     @info @sprintf "%5s  %9s  %7s  %7s " "iter" "f" "‖∇f‖" "σ"
     @info @sprintf "%5d  %9.2e  %7.1e  %7.1e" stats.iter stats.objective norm_∇fk σk
   end
-  if verbose > 0 && mod(output.iter, verbose) == 0
+  if verbose > 0 && mod(stats.iter, verbose) == 0
     @info @sprintf "%5s  %9s  %7s  %7s " "iter" "f" "‖∇f‖" "σ"
     infoline = @sprintf "%5d  %9.2e  %7.1e  %7.1e" stats.iter stats.objective norm_∇fk σk
   end
@@ -132,11 +146,7 @@ function SolverCore.solve!(
 
   callback(nlp, solver, stats)
 
-  done =
-    (stats.status == :first_order) ||
-    (stats.status == :max_eval) ||
-    (stats.status == :max_time) ||
-    (stats.status == :user)
+  done = stats.status != :unknown
 
   while !done
     ck .= x .- (∇fk ./ σk)
@@ -167,12 +177,12 @@ function SolverCore.solve!(
 
     set_iter!(stats, stats.iter + 1)
     set_time!(stats, time() - start_time)
-    set_residuals!(stats, zero(T), norm_∇fk)
+    set_dual_residual!(stats, norm_∇fk)
     optimal = norm_∇fk ≤ ϵ
 
-    if verbose > 0 && mod(output.iter, verbose) == 0
+    if verbose > 0 && mod(stats.iter, verbose) == 0
       @info infoline
-      infoline = @sprintf "%5d  %9.2e  %7.1e  %7.1e" output.iter output.objective norm_∇fk σk
+      infoline = @sprintf "%5d  %9.2e  %7.1e  %7.1e" stats.iter stats.objective norm_∇fk σk
     end
 
     set_status!(
@@ -188,11 +198,7 @@ function SolverCore.solve!(
 
     callback(nlp, solver, stats)
 
-    done =
-      (stats.status == :first_order) ||
-      (stats.status == :max_eval) ||
-      (stats.status == :max_time) ||
-      (stats.status == :user)
+    done = stats.status != :unknown
   end
 
   return stats
