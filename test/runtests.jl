2e2c9f8e4ffc97b6c462d3583435f4a620c2865e
tmigot <tangi.migot@gmail.com>
2023-01-17 19:54:28 +0100

    Add more keywords to `TronSolver`

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,13 +20,14 @@ include("objgrad-on-tron.jl")
 
 @testset "Test max_radius in TRON" begin
   max_radius = 0.00314
+  increase_factor = 5.
   function cb(nlp, solver, stats)
     @test solver.tr.radius ≤ max_radius
   end
 
   nlp = ADNLPModel(x -> 100 * (x[2] - x[1]^2)^2 + (x[1] - 1)^2, [-1.2; 1.0])
-  stats = tron(nlp, max_radius = max_radius, callback = cb)
+  stats = tron(nlp, max_radius = max_radius, increase_factor = increase_factor, callback = cb)
 
   nls = ADNLSModel(x -> [100 * (x[2] - x[1]^2); x[1] - 1], [-1.2; 1.0], 2)
-  stats = tron(nls, max_radius = max_radius, callback = cb)
+  stats = tron(nls, max_radius = max_radius, increase_factor = increase_factor, callback = cb)
 end
