058a07db43d26f5ce2f788162eb728b7177b7fef
tmigot <tangi.migot@gmail.com>
2023-01-26 15:33:01 +0100

    Use test problems in allocation tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,13 @@ using Printf, LinearAlgebra, Logging, SparseArrays, Test
 
 # additional packages
 using ADNLPModels, Krylov, LinearOperators, NLPModels, NLPModelsModifiers, SolverCore, SolverTools
+using NLPModelsTest
 
 # this package
 using JSOSolvers
 
 include("restart.jl")
 include("callback.jl")
-include("simple_model.jl")
 include("consistency.jl")
 include("test_solvers.jl")
 if VERSION ≥ v"1.6"
