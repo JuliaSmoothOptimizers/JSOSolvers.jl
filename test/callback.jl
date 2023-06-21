26ae0ac3276506b27bf880d7ca4e8af4529edc2f
Farhad Rahbarnia <31899325+farhadrclass@users.noreply.github.com>
2023-06-21 18:48:46 -0400

    Update test/callback.jl
    Co-authored-by: tmigot <tangi.migot@gmail.com>

--- test/callback.jl
+++ test/callback.jl
@@ -56,8 +56,6 @@ end
 @testset "Testing Solver Values" begin
   f(x) = (x[1] - 1)^2 + 4 * (x[2] - x[1]^2)^2
   nlp = ADNLPModel(f, [-1.2; 1.0])
-  X = [nlp.meta.x0[1]]
-  Y = [nlp.meta.x0[2]]
   function cb(nlp, solver, stats)
     if stats.iter == 4
       @test solver.σ > 0.0
