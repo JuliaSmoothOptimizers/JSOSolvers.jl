5d4f2c37468c7d55076d59c3014c25650fee32e1
Jay-sanjay <134289328+Jay-sanjay@users.noreply.github.com>
2023-12-17 18:53:02 +0530

    looks more compact.
    Co-authored-by: tmigot <tangi.migot@gmail.com>

--- src/tronls.jl
+++ src/tronls.jl
@@ -236,7 +236,7 @@ function SolverCore.solve!(
     error("tron should only be called for unconstrained or bound-constrained problems")
   end
 
-  if(verbose > 0)
+  if (verbose > 0 && !(u > x > ℓ))
     @warn "Warning: Initial guess is not within bounds."
   end
 
