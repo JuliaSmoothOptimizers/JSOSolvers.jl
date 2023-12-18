299f128bccf4fb4189ec605157df64a1b4a841a2
tmigot <tangi.migot@gmail.com>
2023-12-18 12:13:29 +0100

    Update tron.jl

--- src/tron.jl
+++ src/tron.jl
@@ -217,14 +217,14 @@ function SolverCore.solve!(
     error("tron should only be called for unconstrained or bound-constrained problems")
   end
 
-  if (verbose > 0 && !(u > x > ℓ))
-    @warn "Warning: Initial guess is not within bounds."
-  end
-
   reset!(stats)
   ℓ = nlp.meta.lvar
   u = nlp.meta.uvar
   n = nlp.meta.nvar
+
+  if (verbose > 0 && !(u ≥ x ≥ ℓ))
+    @warn "Warning: Initial guess is not within bounds."
+  end
 
   start_time = time()
   set_time!(stats, 0.0)
