943d788a969d74bcc62eb02d7ffb2230cd0f4b71
d-monnet <monnetdo@gmail.com>
2024-01-25 12:33:40 -0500

    fix test

--- test/test_solvers.jl
+++ test/test_solvers.jl
@@ -8,7 +8,8 @@ function tests()
         ("lbfgs", lbfgs),
         ("tron", tron),
         ("R2", R2),
-        ("fomo", fomo),
+        ("fomo_r2", fomo),
+        ("fomo_tr", (nlp; kwargs...) -> fomo(nlp,backend = JSOSolvers.tr(); kwargs...)),
       ]
         unconstrained_nlp(solver)
         multiprecision_nlp(solver, :unc)
