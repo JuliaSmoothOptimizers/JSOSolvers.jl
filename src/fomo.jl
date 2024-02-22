5d9d12d9fd283e88df9b37a8c8d9bf9ad1fdb925
d-monnet <70266099+d-monnet@users.noreply.github.com>
2024-02-21 23:07:13 -0500

    Update src/fomo.jl
    Co-authored-by: Dominique <dominique.orban@gmail.com>

--- src/fomo.jl
+++ src/fomo.jl
@@ -35,7 +35,7 @@ Extra keyword arguments `σmin` is accepted (`αmax` will be set to `1/σmin`).
 - `γ1 = T(1/2)`, `γ2 = T(2)`: regularization/trust region update parameters.
 - `γ3 = T(1/2)` : momentum factor βmax update parameter in case of unsuccessful iteration.
 - `αmax = 1/eps(T)`: maximum step parameter for fomo solver.
-- `max_eval::Int = -1`: maximum number of evaluation of the objective function.
+- `max_eval::Int = -1`: maximum number of evaluation of the objective function (-1 means unlimited).
 - `max_time::Float64 = 30.0`: maximum time limit in seconds.
 - `max_iter::Int = typemax(Int)`: maximum number of iterations.
 - `β = T(0.9) ∈ [0,1)` : target decay rate for the momentum.
