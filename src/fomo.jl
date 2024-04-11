b5180cd493bbd6255dfb22563250c27f5152b468
d-monnet <monnetdo@gmail.com>
2024-04-11 09:03:46 -0400

    update docstring

--- src/fomo.jl
+++ src/fomo.jl
@@ -22,7 +22,7 @@ and βmax ∈ [0,β] chosen as to ensure d is gradient-related, i.e., the follow
 ‖∇f(xk)‖ ≥ θ2 * ‖(1-βmax) *. ∇f(xk) + βmax .* mk‖       (2)
 In the nonmonotone case, (1) rewrites
 (1-βmax) .* ∇f(xk) + βmax .* ∇f(xk)ᵀmk + (fm - fk)/μk ≥ θ1 * ‖∇f(xk)‖²,
-with fm the greatest objective value over the last M successful iterations, and fk = f(xk).
+with fm the largest objective value over the last M successful iterations, and fk = f(xk).
 
 # Advanced usage
 
