1a4862c9d07f75bce42cbc9d03d1e2cd7b8e3a09
Farhad Rahbarnia <31899325+farhadrclass@users.noreply.github.com>
2026-02-06 13:46:47 -0500

    Update src/R2NLS.jl
    Co-authored-by: Dominique <dominique.orban@gmail.com>

--- src/R2NLS.jl
+++ src/R2NLS.jl
@@ -14,7 +14,7 @@ Parameter set for the R2NLS solver. Controls algorithmic tolerances and step acc
 - `η2 = T(0.95)`: Step is very successful if reduction ≥ η2 (0 < η1 ≤ η2 < 1).
 - `θ1 = T(0.5)`: Controls Cauchy step size (0 < θ1 < 1).
 - `θ2 = eps(T)^(-1)`: Maximum allowed ratio between the step and the Cauchy step (θ2 > 1).
-- `γ1 = T(1.5)`: Increase regularization if step is not good (1 < γ1 ≤ γ2).
+- `γ1 = T(1.5)`: Regularization increase factor on successful (but not very successful) step (1 < γ1 ≤ γ2).
 - `γ2 = T(2.5)`: Further increase if step is rejected (γ1 ≤ γ2).
 - `γ3 = T(0.5)`: Decrease regularization if step is very good (0 < γ3 ≤ 1).
 - `δ1 = T(0.5)`: Cauchy point scaling (0 < δ1 < 1).
